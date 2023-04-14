99e64385ffc76c331f2e02fc3c98a0a5464a597e
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-04-14 15:54:40 +0800

    Add modularized imports

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  modularizeImports: {
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
+    },
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
+    },
+    "@mui/styles": {
+      transform: "@mui/styles/{{member}}",
+    },
+    "@mui/lab": {
+      transform: "@mui/lab/{{member}}",
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
